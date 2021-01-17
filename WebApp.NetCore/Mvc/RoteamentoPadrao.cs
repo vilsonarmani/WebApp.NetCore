15804d324e3e85ab67d4439ea9bb71c2d5e78294
vilsonpereira <vilsonpereira@nasajon.com.br>
2021-01-17 15:31:24 -0300

    WebApp - NetCore Adicionando MVC nativo e tirando as dependencias e alterando rotas

---
+++ WebApp.NetCore/Mvc/RoteamentoPadrao.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace WebApp.NetCore.Mvc
+{
+    public class RoteamentoPadrao
+    {        
+        public static Task TratamentoPadrao(HttpContext context)
+        {
+            //rota padrão: /<Classe>Logica/Metodo
+            //{classe}/{method}
+
+            var classe = Convert.ToString(context.GetRouteValue("classe"));
+            var nomeMetodo = Convert.ToString(context.GetRouteValue("metodo"));
+
+            var nomeCompleto = $"Alura.ListaLeitura.App.Logica.{classe}.Logica";
+
+            var tipo = Type.GetType(classe); //REFLECTION 
+
+            var metodo = tipo.GetMethods().Where(x => x.Name == nomeMetodo).First();
+
+            var requestDelegate = (RequestDelegate)Delegate.CreateDelegate(typeof(RequestDelegate), metodo);
+
+            return requestDelegate.Invoke(context);
+
+
+        }
+    }
+}
--- WebApp.NetCore/Startup.cs
+++ WebApp.NetCore/Startup.cs
@@ -1,6 +1,4 @@
-﻿using Alura.ListaLeitura.App.Logica;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ListaLeitura.App
@@ -12,50 +10,14 @@ namespace Alura.ListaLeitura.App
         {
             //injetando serviço de roteamento
             services.AddRouting();
+            services.AddMvc();//adicionar o MVC
         }
 
         /* MAPEAMENTO DAS ROTAS*/
         public void Configure(IApplicationBuilder app)
         {
-            var builder = new RouteBuilder(app);
-            //construindo as rotas
-            builder.MapRoute("Livros/ParaLer", LivrosLogica.LivrosPraLer);
-            builder.MapRoute("Livros/Lendo", LivrosLogica.LivrosLendo);
-            builder.MapRoute("Livros/Lidos", LivrosLogica.LivrosLidos);
-            builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivroPraLer);
-            builder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.ExibeDetalhes);
-            builder.MapRoute("Cadastro/NovoLivro", CadastroLogica.ExibeFormulario);
-            builder.MapRoute("Cadastro/Incluir", CadastroLogica.ProcessaFormulario);
-
-            var rotas = builder.Build();
-
-            app.UseRouter(rotas);
-
-            //app.Run(Roteamento);
+            app.UseMvcWithDefaultRoute();
         }
-        
-/*
-        public Task Roteamento(HttpContext context)
-        { //roteamento rudimentar
-            var _repo = new LivroRepositorioCSV();
-            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
-            {
-                { "/Livros/ParaLer", LivrosPraLer },
-                { "/Livros/Lendo", LivrosLendo },
-                { "/Livros/Lidos", LivrosLidos }
-            };
-
-            if (caminhosAtendidos.ContainsKey(context.Request.Path))
-            {
-                var metodo = caminhosAtendidos[context.Request.Path];
-                return metodo.Invoke(context);
-            }
-
-            
-            context.Response.StatusCode = 404;
-
-            return context.Response.WriteAsync("Caminho inexistente!");
-
-        }*/       
+    
     }
 }
