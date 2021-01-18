a36284a5e9fb6e557c72687b24d975caa6bca025
VILSON LUIZ PEREIRA <vilsonarmani@gmail.com>
2021-01-17 21:15:08 -0300

    WebApp - NetCore (#4)
    Utilização do Razor para renderizar os dados no html e uso da ViewBag
    
    Co-authored-by: vilsonpereira <vilsonpereira@nasajon.com.br>

--- WebApp.NetCore/Logica/CadastroController.cs
+++ WebApp.NetCore/Logica/CadastroController.cs
@@ -1,27 +1,25 @@
-﻿using Alura.ListaLeitura.App.HTML;
-using Alura.ListaLeitura.App.Negocio;
+﻿using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.ListaLeitura.App.Logica
 {
-    public class CadastroController
+    public class CadastroController : Controller
     {
-         public static Task ExibeFormulario(HttpContext context)
-        {
-            var html = HTMLUtils.CarregaArquivoHTML("formulario");
-            return context.Response.WriteAsync(html);
-        }
 
         public string Incluir(Livro livro)
         {
-           
             var repo = new LivroRepositorioCSV();
-
             repo.Incluir(livro);
+            return "O livro foi adicionado com sucesso";
+        }
 
-            return "O Livro foi Adicionado com sucesso";
+        public IActionResult ExibeFormulario()
+        {
+            //var html = HtmlUtils.CarregaArquivoHTML("formulario");
+            //var html = new ViewResult { ViewName = "formulario" };
+
+            return View("formulario");
         }
     }
 }
